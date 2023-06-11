bf53201c37d9535a4bc9e55ef74f754899f0e8d2
clown0123 <115709696+clown0123@users.noreply.github.com>
2023-06-11 21:49:50 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,16 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package penjualan;
-
 import barang.Mobil;
 import java.util.List;
 
-/**
- *
- * @author budi
- */
 public class Main {
     public static void main(String[] args) {
         PenjualanMobil penjualanMobil = new PenjualanMobil();
@@ -22,7 +12,7 @@ public class Main {
         Mobil mobil2 = new Mobil("Honda", "Civic", 2022, 200000000);
         penjualanMobil.beliMobil(mobil2);
 
-         // Jual mobil dengan harga jual tertentu
+        // Jual mobil dengan harga jual tertentu
         double hargaJualMobil2 = 210000000;
         penjualanMobil.jualMobil(mobil2, hargaJualMobil2);
 
@@ -38,3 +28,4 @@ public class Main {
         }
     }
 }
+
