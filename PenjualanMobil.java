f5b35fe3dfa896d17fdd2cf6e5e5b3638245dd4d
clown0123 <115709696+clown0123@users.noreply.github.com>
2023-06-11 21:51:44 +0700

    Update PenjualanMobil.java

--- PenjualanMobil.java
+++ PenjualanMobil.java
@@ -1,13 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
-package penjualan;
-
-/**
- *
- * @author budi
- */
 import barang.Mobil;
 import java.util.ArrayList;
 import java.util.List;
@@ -17,24 +7,42 @@ public class PenjualanMobil {
     private double uangModal = 1000000000.0;
     private double nilaiAsset = 500000000.0;
     
-
     public PenjualanMobil() {
         daftarMobil = new ArrayList<>();
-        int uangModal = (int) 1000000000.0;
-        int nilaiAsset = (int) 500000000.0;
-        
     }
 
-    public void beliMobil(Mobil mobil) {
+    public void beliMobil(String merek, String model, int tahun, double harga) {
+        beliMobil(merek);
+    }
+
+    public void beliMobil(String merek) {
+        beliMobil(merek);
+    }
+
+
+   public void beliMobil(Mobil mobil) {
+    if (uangModal >= mobil.getHarga()) {
         daftarMobil.add(mobil);
         uangModal -= mobil.getHarga();
         nilaiAsset += mobil.getHarga();
-    }
+       System.out.println("Berhasil membeli mobil " + mobil.getMerk());
+  } else {
+    System.out.println("Uang modal tidak mencukupi untuk membeli mobil " + mobil.getMerk());
+}
+
+
+}
 
     public void jualMobil(Mobil mobil, double hargaJual) {
-        daftarMobil.remove(mobil);
-        uangModal += hargaJual;
-        nilaiAsset -= mobil.getHarga();
+        if (daftarMobil.contains(mobil)) {
+            daftarMobil.remove(mobil);
+            uangModal += hargaJual;
+            nilaiAsset -= mobil.getHarga();
+            System.out.println("Berhasil menjual mobil " + mobil.getMerk() + " dengan harga " + hargaJual);
+} else {
+    System.out.println("Mobil tidak ada dalam daftar");
+}
+
     }
     
     public List<Mobil> getDaftarMobil() {
@@ -52,11 +60,11 @@ public class PenjualanMobil {
     public List<Mobil> cariMobilBerdasarkanMerek(String merek) {
         List<Mobil> hasilPencarian = new ArrayList<>();
         for (Mobil mobil : daftarMobil) {
-            if (mobil.getMerek().equalsIgnoreCase(merek)) {
+            if (mobil.getMerk().equalsIgnoreCase(merek)) {
                 hasilPencarian.add(mobil);
             }
         }
         return hasilPencarian;
     }
-    // Metode lain untuk mencari mobil berdasarkan kriteria lainnya
 }
+
